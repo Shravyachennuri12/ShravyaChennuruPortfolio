3528bb6b37c1f4735f0efabd63b4cec6ddd2a549
Shravyachennuri12 <chennurishravya088@gmail.com>
2025-04-21 10:27:17 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+
+
+  // reveal on scroll
+const reveals = document.querySelectorAll(".reveal");
+
+const observer = new IntersectionObserver(
+  entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("active");
+      }
+    });
+  },
+  { threshold: 0.1 }
+);
+
+reveals.forEach(reveal => {
+  observer.observe(reveal);
+});
+
